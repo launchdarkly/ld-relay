278ed0c0121266ce5bc51013b59c18c9f7601599
Arnold Trakhtenberg <arnoldtrakh@gmail.com>
2018-02-01 16:13:11 -0800

    Don't start an http server in tests

--- ld-relay_test.go
+++ ld-relay_test.go
@@ -41,26 +41,14 @@ func handler() muxHandler {
 	return muxHandler{clients: clients}
 }
 
-func testMethod(verb string, vars map[string]string, headers map[string]string, body string, method func(w http.ResponseWriter, r *http.Request)) *http.Response {
-	router := mux.NewRouter()
-	matcher := ""
-	urlString := ""
-	for k, v := range vars {
-		matcher = matcher + fmt.Sprintf("/{%s}", k)
-		urlString = urlString + fmt.Sprintf("/%s", v)
-	}
-
-	router.HandleFunc(matcher, func(w http.ResponseWriter, r *http.Request) {
-		method(w, r)
-	})
-
-	server := httptest.NewServer(router)
-	req, _ := http.NewRequest(verb, server.URL+urlString, bytes.NewBuffer([]byte(body)))
+func testMethod(verb string, vars map[string]string, headers map[string]string, body string, method func(w http.ResponseWriter, r *http.Request)) *httptest.ResponseRecorder {
+	req, _ := http.NewRequest(verb, "", bytes.NewBuffer([]byte(body)))
+	mux.SetURLVars(req, vars)
 	for k, v := range headers {
 		req.Header.Set(k, v)
 	}
-	client := http.Client{}
-	resp, _ := client.Do(req)
+	resp := httptest.NewRecorder()
+	method(resp, req)
 	return resp
 }
 
@@ -69,9 +57,8 @@ func TestGetFlagEvalSucceeds(t *testing.T) {
 	headers := map[string]string{"Authorization": key()}
 	resp := testMethod("GET", vars, headers, "", handler().authorizeEval(evaluateAllFeatureFlags))
 
-	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	assert.Equal(t, http.StatusOK, resp.Code)
 
-	defer resp.Body.Close()
 	b, _ := ioutil.ReadAll(resp.Body)
 
 	assert.Equal(t, `{"another-flag-key":3,"some-flag-key":true}`, string(b))
@@ -82,9 +69,8 @@ func TestReportFlagEvalSucceeds(t *testing.T) {
 	headers := map[string]string{"Authorization": key(), "Content-Type": "application/json"}
 	resp := testMethod("REPORT", vars, headers, `{"user":"key"}`, handler().authorizeEval(evaluateAllFeatureFlags))
 
-	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	assert.Equal(t, http.StatusOK, resp.Code)
 
-	defer resp.Body.Close()
 	b, _ := ioutil.ReadAll(resp.Body)
 
 	assert.Equal(t, `{"another-flag-key":3,"some-flag-key":true}`, string(b))
@@ -95,7 +81,7 @@ func TestFlagEvalFailsOnInvalidAuthKey(t *testing.T) {
 	headers := map[string]string{"Authorization": "mob-eeeeeeee-eeee-4eee-aeee-eeeeeeeeeeee", "Content-Type": "application/json"}
 	resp := testMethod("REPORT", vars, headers, `{"user":"key"}`, handler().authorizeEval(evaluateAllFeatureFlags))
 
-	assert.Equal(t, http.StatusUnauthorized, resp.StatusCode)
+	assert.Equal(t, http.StatusUnauthorized, resp.Code)
 }
 
 func TestFlagEvalFailsOnInvalidUserJson(t *testing.T) {
@@ -103,16 +89,15 @@ func TestFlagEvalFailsOnInvalidUserJson(t *testing.T) {
 	headers := map[string]string{"Authorization": key(), "Content-Type": "application/json"}
 	resp := testMethod("REPORT", vars, headers, `{"user":"key"}notjson`, handler().authorizeEval(evaluateAllFeatureFlags))
 
-	assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
+	assert.Equal(t, http.StatusBadRequest, resp.Code)
 }
 
 func TestClientSideFlagEvalSucceeds(t *testing.T) {
 	vars := map[string]string{"envId": key(), "user": user()}
 	resp := testMethod("GET", vars, nil, "", handler().findEnvironment(evaluateAllFeatureFlags))
 
-	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	assert.Equal(t, http.StatusOK, resp.Code)
 
-	defer resp.Body.Close()
 	b, _ := ioutil.ReadAll(resp.Body)
 
 	assert.Equal(t, `{"another-flag-key":3,"some-flag-key":true}`, string(b))
@@ -123,5 +108,5 @@ func TestClientSideFlagEvalFailsOnInvalidEnvId(t *testing.T) {
 	headers := map[string]string{"Content-Type": "application/json"}
 	resp := testMethod("GET", vars, headers, "", handler().findEnvironment(evaluateAllFeatureFlags))
 
-	assert.Equal(t, http.StatusNotFound, resp.StatusCode)
+	assert.Equal(t, http.StatusNotFound, resp.Code)
 }
