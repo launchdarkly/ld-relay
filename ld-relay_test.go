ec6be1ff331253e17c10ec060803e0d446135979
Arnold Trakhtenberg <arnoldtrakh@gmail.com>
2018-02-05 15:56:55 -0800

    Update tests to review spec

--- ld-relay_test.go
+++ ld-relay_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/stretchr/testify/mock"
 
 	"bytes"
+	"context"
 	ld "gopkg.in/launchdarkly/go-client.v2"
 	"io/ioutil"
 	"net/http"
@@ -40,21 +41,22 @@ func handler() muxHandler {
 	return muxHandler{clients: clients}
 }
 
-func testMethod(verb string, vars map[string]string, headers map[string]string, body string, method func(w http.ResponseWriter, r *http.Request)) *httptest.ResponseRecorder {
+func buildRequest(verb string, vars map[string]string, headers map[string]string, body string) *http.Request {
 	req, _ := http.NewRequest(verb, "", bytes.NewBuffer([]byte(body)))
 	req = mux.SetURLVars(req, vars)
 	for k, v := range headers {
 		req.Header.Set(k, v)
 	}
-	resp := httptest.NewRecorder()
-	method(resp, req)
-	return resp
+	ctx := evalContextImpl{client: &FakeLDClient{}}
+	req = req.WithContext(context.WithValue(req.Context(), "context", ctx))
+	return req
 }
 
 func TestGetFlagEvalSucceeds(t *testing.T) {
 	vars := map[string]string{"user": user()}
-	headers := map[string]string{"Authorization": key()}
-	resp := testMethod("GET", vars, headers, "", handler().authorizeMethod(evaluateAllFeatureFlags))
+	req := buildRequest("GET", vars, nil, "")
+	resp := httptest.NewRecorder()
+	evaluateAllFeatureFlags(resp, req)
 
 	assert.Equal(t, http.StatusOK, resp.Code)
 
@@ -65,8 +67,10 @@ func TestGetFlagEvalSucceeds(t *testing.T) {
 
 func TestReportFlagEvalSucceeds(t *testing.T) {
 	vars := map[string]string{"user": user()}
-	headers := map[string]string{"Authorization": key(), "Content-Type": "application/json"}
-	resp := testMethod("REPORT", vars, headers, `{"user":"key"}`, handler().authorizeMethod(evaluateAllFeatureFlags))
+	headers := map[string]string{"Content-Type": "application/json"}
+	req := buildRequest("REPORT", vars, headers, `{"user":"key"}`)
+	resp := httptest.NewRecorder()
+	evaluateAllFeatureFlags(resp, req)
 
 	assert.Equal(t, http.StatusOK, resp.Code)
 
@@ -78,34 +82,28 @@ func TestReportFlagEvalSucceeds(t *testing.T) {
 func TestAuthorizeMethodFailsOnInvalidAuthKey(t *testing.T) {
 	vars := map[string]string{"user": user()}
 	headers := map[string]string{"Authorization": "mob-eeeeeeee-eeee-4eee-aeee-eeeeeeeeeeee", "Content-Type": "application/json"}
-	resp := testMethod("REPORT", vars, headers, `{"user":"key"}`, handler().authorizeMethod(evaluateAllFeatureFlags))
+	req := buildRequest("REPORT", vars, headers, `{"user":"key"}`)
+	resp := httptest.NewRecorder()
+	handler().authorizeMethod(func(http.ResponseWriter, *http.Request) { t.Fail() })(resp, req)
 
 	assert.Equal(t, http.StatusUnauthorized, resp.Code)
 }
 
 func TestFlagEvalFailsOnInvalidUserJson(t *testing.T) {
 	vars := map[string]string{"user": user()}
-	headers := map[string]string{"Authorization": key(), "Content-Type": "application/json"}
-	resp := testMethod("REPORT", vars, headers, `{"user":"key"}notjson`, handler().authorizeMethod(evaluateAllFeatureFlags))
+	headers := map[string]string{"Content-Type": "application/json"}
+	req := buildRequest("REPORT", vars, headers, `{"user":"key"}notjson`)
+	resp := httptest.NewRecorder()
+	evaluateAllFeatureFlags(resp, req)
 
 	assert.Equal(t, http.StatusBadRequest, resp.Code)
 }
 
-func TestClientSideFlagEvalSucceeds(t *testing.T) {
-	vars := map[string]string{"envId": key(), "user": user()}
-	resp := testMethod("GET", vars, nil, "", handler().findEnvironment(evaluateAllFeatureFlags))
-
-	assert.Equal(t, http.StatusOK, resp.Code)
-
-	b, _ := ioutil.ReadAll(resp.Body)
-
-	assert.Equal(t, `{"another-flag-key":3,"some-flag-key":true}`, string(b))
-}
-
 func TestFindEnvironmentFailsOnInvalidEnvId(t *testing.T) {
 	vars := map[string]string{"envId": "blah", "user": user()}
-	headers := map[string]string{"Content-Type": "application/json"}
-	resp := testMethod("GET", vars, headers, "", handler().findEnvironment(evaluateAllFeatureFlags))
+	req := buildRequest("GET", vars, nil, "")
+	resp := httptest.NewRecorder()
+	handler().findEnvironment(evaluateAllFeatureFlags)(resp, req)
 
 	assert.Equal(t, http.StatusNotFound, resp.Code)
 }
