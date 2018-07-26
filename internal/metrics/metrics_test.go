e433616acdc36c87e73427802298b5f22d0ccbba
Arnold Trakhtenberg <arnoldtrakh@gmail.com>
2018-07-26 14:33:16 -0700

    Flatten metrics tests

--- internal/metrics/metrics_test.go
+++ internal/metrics/metrics_test.go
@@ -3,6 +3,7 @@ package metrics
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"testing"
 	"time"
 
@@ -11,6 +12,7 @@ import (
 	"go.opencensus.io/trace"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 var (
@@ -45,6 +47,13 @@ type args struct {
 	route     string
 }
 
+func (a args) getExpectedTags() []tag.Tag {
+	if a.method == "" && a.route == "" {
+		return []tag.Tag{tag.Tag{Key: platformCategoryTagKey, Value: a.platform}, tag.Tag{Key: relayIdTagKey, Value: a.relayId}, tag.Tag{Key: userAgentTagKey, Value: a.userAgent}}
+	}
+	return []tag.Tag{tag.Tag{Key: methodTagKey, Value: a.method}, tag.Tag{Key: platformCategoryTagKey, Value: a.platform}, tag.Tag{Key: routeTagKey, Value: a.route}, tag.Tag{Key: userAgentTagKey, Value: a.userAgent}}
+}
+
 func TestConnectionMetrics(t *testing.T) {
 	specs := []args{
 		args{value: 1, platform: browser, measure: BrowserConns, relayId: metricsRelayId, userAgent: userAgentValue},
@@ -60,7 +69,11 @@ func TestConnectionMetrics(t *testing.T) {
 		}
 		defer p.Close()
 		WithGauge(p.OpenCensusCtx, userAgentValue, func() {
-			equalCheck(t, tt, "connections")
+			expectedTags := tt.getExpectedTags()
+			rows, _ := view.RetrieveData("connections")
+			matchingRows := findRowsWithTags(rows, expectedTags)
+			require.Len(t, matchingRows, 1)
+			assert.Equal(t, float64((*matchingRows[0]).Data.(*view.SumData).Value), tt.value)
 		}, tt.measure)
 
 	}
@@ -81,7 +94,11 @@ func TestNewConnectionMetrics(t *testing.T) {
 		}
 		defer p.Close()
 		WithCount(p.OpenCensusCtx, userAgentValue, func() {
-			equalCheck(t, tt, "newconnections")
+			expectedTags := tt.getExpectedTags()
+			rows, _ := view.RetrieveData("newconnections")
+			matchingRows := findRowsWithTags(rows, expectedTags)
+			require.Len(t, matchingRows, 1)
+			assert.Equal(t, float64((*matchingRows[0]).Data.(*view.SumData).Value), tt.value)
 		}, tt.measure)
 	}
 }
@@ -126,33 +143,26 @@ func TestWithRouteCount(t *testing.T) {
 	expected := args{value: 1, platform: server, measure: NewServerConns, userAgent: userAgentValue, method: "GET", route: "someRoute"}
 
 	WithRouteCount(context.Background(), userAgentValue, "someRoute", "GET", func() {
-		routeEqualCheck(t, expected, "requests")
+		expectedTags := expected.getExpectedTags()
+		rows, _ := view.RetrieveData("requests")
+		matchingRows := findRowsWithTags(rows, expectedTags)
+		require.Len(t, matchingRows, 1)
+		assert.Equal(t, int64((*matchingRows[0]).Data.(*view.CountData).Value), int64(expected.value))
 	}, ServerRequests)
 	assert.NotEmpty(t, exporter.spans)
 }
 
-func routeEqualCheck(t *testing.T, expected args, viewName string) {
-	expectedRow := &view.Row{Tags: []tag.Tag{tag.Tag{Key: methodTagKey, Value: expected.method}, tag.Tag{Key: platformCategoryTagKey, Value: expected.platform}, tag.Tag{Key: routeTagKey, Value: expected.route}, tag.Tag{Key: userAgentTagKey, Value: expected.userAgent}}, Data: &view.CountData{Value: int64(expected.value)}}
-	rowEqualCheck(t, expectedRow, viewName)
-}
-
-func equalCheck(t *testing.T, expected args, viewName string) {
-	expectedRow := &view.Row{Tags: []tag.Tag{tag.Tag{Key: platformCategoryTagKey, Value: expected.platform}, tag.Tag{Key: relayIdTagKey, Value: expected.relayId}, tag.Tag{Key: userAgentTagKey, Value: expected.userAgent}}, Data: &view.SumData{Value: expected.value}}
-	rowEqualCheck(t, expectedRow, viewName)
-}
-
-func rowEqualCheck(t *testing.T, expected *view.Row, viewName string) {
-	rows, _ := view.RetrieveData(viewName)
-
-	found := false
-EqualCheck:
+func findRowsWithTags(rows []*view.Row, tags []tag.Tag) (matches []*view.Row) {
+	fmt.Println(rows)
+	fmt.Println(tags)
+RowLoop:
 	for _, row := range rows {
-		if expected.Equal(row) {
-			found = true
-			break EqualCheck
+		for i := range row.Tags {
+			if row.Tags[i] != tags[i] {
+				continue RowLoop
+			}
 		}
+		matches = append(matches, row)
 	}
-	if !assert.True(t, found) {
-		t.Logf("%+v does not contain\n%+v", rows, expected)
-	}
+	return matches
 }
