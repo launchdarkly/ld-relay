8f0731e4a1cf54866311343ad4a9bc7107501078
Eli Bishop <eli@launchdarkly.com>
2019-07-10 12:21:37 -0700

    add ability to specify CA certs; remove InsecureSkipVerify parameter

--- httpconfig/httpconfig.go
+++ httpconfig/httpconfig.go
@@ -2,12 +2,15 @@ package httpconfig
 
 import (
 	"crypto/tls"
+	"crypto/x509"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
 	"net/url"
+	"strings"
 	"time"
 
 	ntlm "github.com/Codehardt/go-ntlm-proxy-auth"
@@ -17,17 +20,19 @@ import (
 
 // ProxyConfig represents all the supported proxy options. This is used in the Config struct in relay.go.
 type ProxyConfig struct {
-	Url           string
-	NtlmAuth      bool
-	User          string
-	Password      string
-	Domain        string
-	SkipTlsVerify bool
+	Url         string
+	NtlmAuth    bool
+	User        string
+	Password    string
+	Domain      string
+	CaCertFiles string
 }
 
 // HTTPConfig encapsulates ProxyConfig plus any other HTTP options we may support in the future (currently none).
 type HTTPConfig struct {
 	ProxyConfig
+	ProxyURL *url.URL
+	CaCerts  *x509.CertPool
 }
 
 const defaultTimeout = 10 * time.Second
@@ -35,21 +40,35 @@ const defaultTimeout = 10 * time.Second
 // NewHTTPConfig validates all of the HTTP-related options and returns an HTTPConfig if successful.
 func NewHTTPConfig(proxyConfig ProxyConfig) (HTTPConfig, error) {
 	ntlm.SetDebugf(log.Printf)
-	ret := HTTPConfig{proxyConfig}
+	ret := HTTPConfig{ProxyConfig: proxyConfig}
 	if proxyConfig.Url == "" && proxyConfig.NtlmAuth {
 		return ret, errors.New("Cannot specify proxy authentication without a proxy URL")
 	}
 	if proxyConfig.Url != "" {
-		if _, err := url.Parse(proxyConfig.Url); err != nil {
+		u, err := url.Parse(proxyConfig.Url)
+		if err != nil {
 			return ret, fmt.Errorf("Invalid proxy URL: %s", proxyConfig.Url)
 		}
 		logging.Info.Printf("Using proxy server at %s", proxyConfig.Url)
+		ret.ProxyURL = u
 	}
 	if proxyConfig.NtlmAuth {
 		if proxyConfig.User == "" || proxyConfig.Password == "" {
 			return ret, errors.New("NTLM proxy authentication requires username and password")
 		}
 		logging.Info.Printf("NTLM proxy authentication enabled")
+	}
+	for _, filePath := range strings.Split(strings.TrimSpace(proxyConfig.CaCertFiles), ",") {
+		bytes, err := ioutil.ReadFile(filePath)
+		if err != nil {
+			return ret, fmt.Errorf("Can't read CA certificate file %s", filePath)
+		}
+		if ret.CaCerts == nil {
+			ret.CaCerts = x509.NewCertPool()
+		}
+		if !ret.CaCerts.AppendCertsFromPEM(bytes) {
+			return ret, fmt.Errorf("CA certificate file %s did not contain a valid certificate", filePath)
+		}
 	}
 	return ret, nil
 }
@@ -67,36 +86,32 @@ func (c HTTPConfig) CreateHTTPClientForSDK(config ld.Config) http.Client {
 
 func (c HTTPConfig) newHTTPClient(timeout time.Duration) http.Client {
 	client := http.Client{}
-	var proxyURL *url.URL
-	if c.ProxyConfig.Url != "" {
-		proxyURL, _ = url.Parse(c.ProxyConfig.Url)
-	}
-	makeProxyTLSConfig := func() *tls.Config {
-		if c.ProxyConfig.SkipTlsVerify {
-			return &tls.Config{
-				InsecureSkipVerify: true,
-			}
+	makeTLSConfig := func() *tls.Config {
+		if c.CaCerts != nil {
+			return &tls.Config{RootCAs: c.CaCerts}
 		}
 		return nil
 	}
 	makeProxyTransport := func() *http.Transport {
-		return &http.Transport{TLSClientConfig: makeProxyTLSConfig()}
+		return &http.Transport{TLSClientConfig: makeTLSConfig()}
 	}
-	if c.ProxyConfig.NtlmAuth {
-		// See: https://github.com/Codehardt/go-ntlm-proxy-auth
-		transport := makeProxyTransport()
-		dialer := &net.Dialer{
-			Timeout:   timeout,
-			KeepAlive: timeout,
+	if c.ProxyURL != nil {
+		if c.ProxyConfig.NtlmAuth {
+			// See: https://github.com/Codehardt/go-ntlm-proxy-auth
+			transport := makeProxyTransport()
+			dialer := &net.Dialer{
+				Timeout:   timeout,
+				KeepAlive: timeout,
+			}
+			transport.DialContext = ntlm.NewNTLMProxyDialContext(dialer, *c.ProxyURL,
+				c.ProxyConfig.User, c.ProxyConfig.Password, c.ProxyConfig.Domain,
+				makeTLSConfig())
+			client.Transport = transport
+		} else {
+			transport := makeProxyTransport()
+			transport.Proxy = http.ProxyURL(c.ProxyURL)
+			client.Transport = transport
 		}
-		transport.DialContext = ntlm.NewNTLMProxyDialContext(dialer, *proxyURL,
-			c.ProxyConfig.User, c.ProxyConfig.Password, c.ProxyConfig.Domain,
-			makeProxyTLSConfig())
-		client.Transport = transport
-	} else if proxyURL != nil {
-		transport := makeProxyTransport()
-		transport.Proxy = http.ProxyURL(proxyURL)
-		client.Transport = transport
 	}
 	return client
 }
