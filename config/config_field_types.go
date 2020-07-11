23d2ad29b37b278ce57a646e6557dcb90ef84942
Eli Bishop <eli@launchdarkly.com>
2020-07-11 09:38:11 -0700

    tests + misc field type improvements

--- config/config_field_types.go
+++ config/config_field_types.go
@@ -49,27 +49,36 @@ func (k EnvironmentID) GetAuthorizationHeaderValue() string {
 // OptAbsoluteURL represents an optional URL parameter which, if present, must be a valid URL. This is enforced
 // by its representation of encoding.TextUnmarshaler, which is called by both the gcfg config file parser
 // and our environment variable logic.
+//
+// The zero value OptAbsoluteURL{} is valid and undefined (IsDefined() is false, Get() is nil).
 type OptAbsoluteURL struct {
 	url *url.URL
 }
 
+// NewOptAbsoluteURLFromURL creates an OptAbsoluteURL from an already-parsed URL. It returns an error if the
+// URL is not an absolute URL. If the parameter is nil, it returns an empty OptAbsoluteURL{}.
 func NewOptAbsoluteURLFromURL(url *url.URL) (OptAbsoluteURL, error) {
 	if url == nil {
 		return OptAbsoluteURL{}, nil
 	}
 	if !url.IsAbs() {
-		return OptAbsoluteURL{}, errors.New("must be an absolute URL/URI")
+		return OptAbsoluteURL{}, errNotAbsoluteURL()
 	}
 	u := *url
 	return OptAbsoluteURL{url: &u}, nil
 }
 
+// NewOptAbsoluteURLFromURL creates an OptAbsoluteURL from a string. It returns an error if the string is not
+// a URL or is a relative URL. If the string is empty, it returns an empty OptAbsoluteURL{}.
 func NewOptAbsoluteURLFromString(urlString string) (OptAbsoluteURL, error) {
+	if urlString == "" {
+		return OptAbsoluteURL{}, nil
+	}
 	u, err := url.Parse(urlString)
 	if err == nil {
 		return NewOptAbsoluteURLFromURL(u)
 	}
-	return OptAbsoluteURL{}, errors.New("not a valid URL/URI")
+	return OptAbsoluteURL{}, errBadURLString()
 }
 
 func newOptAbsoluteURLMustBeValid(urlString string) OptAbsoluteURL {
@@ -80,10 +89,12 @@ func newOptAbsoluteURLMustBeValid(urlString string) OptAbsoluteURL {
 	return o
 }
 
+// IsDefined is true if this instance has a value (Get() is not nil).
 func (o OptAbsoluteURL) IsDefined() bool {
 	return o.url != nil
 }
 
+// Get returns the wrapped URL if any, or nil.
 func (o OptAbsoluteURL) Get() *url.URL {
 	if o.url == nil {
 		return nil
@@ -92,10 +103,12 @@ func (o OptAbsoluteURL) Get() *url.URL {
 	return &u
 }
 
+// String returns the URL converted to a string, or "" if undefined.
 func (o OptAbsoluteURL) String() string {
 	return o.StringOrElse("")
 }
 
+// StringOrElse returns the URL converted to a string, or the alternative value if undefined.
 func (o OptAbsoluteURL) StringOrElse(orElseValue string) string {
 	if o.url == nil {
 		return orElseValue
@@ -103,11 +116,9 @@ func (o OptAbsoluteURL) StringOrElse(orElseValue string) string {
 	return o.url.String()
 }
 
+// UnmarshalText attempts to parse the value from a byte string, using the same logic as
+// NewOptAbsoluteURLFromString.
 func (o *OptAbsoluteURL) UnmarshalText(data []byte) error {
-	if len(data) == 0 {
-		o.url = nil
-		return nil
-	}
 	parsed, err := NewOptAbsoluteURLFromString(string(data))
 	if err == nil {
 		*o = parsed
@@ -115,16 +126,29 @@ func (o *OptAbsoluteURL) UnmarshalText(data []byte) error {
 	return err
 }
 
+func errBadURLString() error {
+	return errors.New("not a valid URL/URI")
+}
+
+func errNotAbsoluteURL() error {
+	return errors.New("must be an absolute URL/URI")
+}
+
 // OptLogLevel represents an optional log level parameter. It must match one of the level names "debug",
 // "info", "warn", or "error" (case-insensitive).
+//
+// The zero value OptLogLevel{} is valid and undefined (IsDefined() is false).
 type OptLogLevel struct {
 	level ldlog.LogLevel
 }
 
+// NewOptLogLevel creates an OptLogLevel that wraps the given value.
 func NewOptLogLevel(level ldlog.LogLevel) OptLogLevel {
 	return OptLogLevel{level: level}
 }
 
+// NewOptLogLevelFromString creates an OptLogLevel from a string that must either be a valid log level
+// name or an empty string.
 func NewOptLogLevelFromString(levelName string) (OptLogLevel, error) {
 	if levelName == "" {
 		return OptLogLevel{}, nil
@@ -134,13 +158,15 @@ func NewOptLogLevelFromString(levelName string) (OptLogLevel, error) {
 			return NewOptLogLevel(level), nil
 		}
 	}
-	return OptLogLevel{}, fmt.Errorf(`"%s" is not a valid log level`, levelName)
+	return OptLogLevel{}, errBadLogLevel(levelName)
 }
 
+// IsDefined returns true if the instance contains a value.
 func (o OptLogLevel) IsDefined() bool {
 	return o.level != 0
 }
 
+// GetOrElse returns the wrapped value, or the alternative value if there is no value.
 func (o OptLogLevel) GetOrElse(orElseValue ldlog.LogLevel) ldlog.LogLevel {
 	if o.level == 0 {
 		return orElseValue
@@ -148,10 +174,16 @@ func (o OptLogLevel) GetOrElse(orElseValue ldlog.LogLevel) ldlog.LogLevel {
 	return o.level
 }
 
+// UnmarshalText attempts to parse the value from a byte string, using the same logic as
+// NewOptLogLevelFromString.
 func (o *OptLogLevel) UnmarshalText(data []byte) error {
 	opt, err := NewOptLogLevelFromString(string(data))
 	if err == nil {
 		*o = opt
 	}
 	return err
+}
+
+func errBadLogLevel(s string) error {
+	return fmt.Errorf("%q is not a valid log level", s)
 }
