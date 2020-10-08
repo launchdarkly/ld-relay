c528d86deca2d0393bfdf41091aa666ece28821f
LaunchDarklyCI <LaunchDarklyCI@users.noreply.github.com>
2020-10-08 21:03:00 +0100

    prepare 6.0.1 release (#106)

--- internal/autoconfig/stream_manager_expiring_key_test.go
+++ internal/autoconfig/stream_manager_expiring_key_test.go
@@ -17,6 +17,24 @@ const (
 	briefExpiryMillis = 300
 )
 
+func makeEnvWithExpiringKey(fromEnv EnvironmentRep, oldKey config.SDKKey) EnvironmentRep {
+	ret := fromEnv
+	ret.SDKKey.Expiring = ExpiringKeyRep{
+		Value:     oldKey,
+		Timestamp: ldtime.UnixMillisNow() + briefExpiryMillis,
+	}
+	return ret
+}
+
+func makeEnvWithAlreadyExpiredKey(fromEnv EnvironmentRep, oldKey config.SDKKey) EnvironmentRep {
+	ret := fromEnv
+	ret.SDKKey.Expiring = ExpiringKeyRep{
+		Value:     oldKey,
+		Timestamp: ldtime.UnixMillisNow() - 1,
+	}
+	return ret
+}
+
 func expectOldKeyWillExpire(p streamManagerTestParams, envID config.EnvironmentID) {
 	p.mockLog.AssertMessageMatch(p.t, true, ldlog.Warn, "Old SDK key ending in dkey .* will expire")
 	assert.Len(p.t, p.mockLog.GetOutput(ldlog.Error), 0)
@@ -29,13 +47,12 @@ func expectOldKeyWillExpire(p streamManagerTestParams, envID config.EnvironmentI
 	p.mockLog.AssertMessageMatch(p.t, true, ldlog.Warn, "Old SDK key ending in dkey .* has expired")
 }
 
-func TestExpiringKeyInPutMessage(t *testing.T) {
-	envWithExpiringKey := testEnv1
-	envWithExpiringKey.SDKKey.Expiring = ExpiringKeyRep{
-		Value:     oldKey,
-		Timestamp: ldtime.UnixMillisNow() + briefExpiryMillis,
-	}
+func expectNoKeyExpiryMessage(p streamManagerTestParams) {
+	p.mockLog.AssertMessageMatch(p.t, false, ldlog.Warn, "Old SDK key .* will expire")
+}
 
+func TestExpiringKeyInPutMessage(t *testing.T) {
+	envWithExpiringKey := makeEnvWithExpiringKey(testEnv1, oldKey)
 	event := makePutEvent(envWithExpiringKey)
 	streamManagerTest(t, &event, func(p streamManagerTestParams) {
 		p.startStream()
@@ -50,12 +67,7 @@ func TestExpiringKeyInPutMessage(t *testing.T) {
 }
 
 func TestExpiringKeyInPatchAdd(t *testing.T) {
-	envWithExpiringKey := testEnv1
-	envWithExpiringKey.SDKKey.Expiring = ExpiringKeyRep{
-		Value:     oldKey,
-		Timestamp: ldtime.UnixMillisNow() + briefExpiryMillis,
-	}
-
+	envWithExpiringKey := makeEnvWithExpiringKey(testEnv1, oldKey)
 	event := makePatchEvent(envWithExpiringKey)
 	streamManagerTest(t, nil, func(p streamManagerTestParams) {
 		p.startStream()
@@ -77,11 +89,7 @@ func TestExpiringKeyInPatchUpdate(t *testing.T) {
 
 		_ = p.requireMessage()
 
-		envWithExpiringKey := testEnv1
-		envWithExpiringKey.SDKKey.Expiring = ExpiringKeyRep{
-			Value:     oldKey,
-			Timestamp: ldtime.UnixMillisNow() + briefExpiryMillis,
-		}
+		envWithExpiringKey := makeEnvWithExpiringKey(testEnv1, oldKey)
 		envWithExpiringKey.Version++
 
 		p.stream.Enqueue(makePatchEvent(envWithExpiringKey))
@@ -92,5 +100,57 @@ func TestExpiringKeyInPatchUpdate(t *testing.T) {
 		assert.Equal(t, oldKey, msg.update.ExpiringSDKKey)
 
 		expectOldKeyWillExpire(p, envWithExpiringKey.EnvID)
+	})
+}
+
+func TestExpiringKeyHasAlreadyExpiredInPutMessage(t *testing.T) {
+	envWithExpiringKey := makeEnvWithAlreadyExpiredKey(testEnv1, oldKey)
+	event := makePutEvent(envWithExpiringKey)
+	streamManagerTest(t, &event, func(p streamManagerTestParams) {
+		p.startStream()
+
+		msg := p.requireMessage()
+		require.NotNil(t, msg.add)
+		assert.Equal(t, makeEnvironmentParams(testEnv1), *msg.add)
+		assert.Equal(t, config.SDKKey(""), msg.add.ExpiringSDKKey)
+
+		expectNoKeyExpiryMessage(p)
+	})
+}
+
+func TestExpiringKeyHasAlreadyExpiredInPatchAdd(t *testing.T) {
+	envWithExpiringKey := makeEnvWithAlreadyExpiredKey(testEnv1, oldKey)
+	event := makePatchEvent(envWithExpiringKey)
+	streamManagerTest(t, nil, func(p streamManagerTestParams) {
+		p.startStream()
+		p.stream.Enqueue(event)
+
+		msg := p.requireMessage()
+		require.NotNil(t, msg.add)
+		assert.Equal(t, makeEnvironmentParams(testEnv1), *msg.add)
+		assert.Equal(t, config.SDKKey(""), msg.add.ExpiringSDKKey)
+
+		expectNoKeyExpiryMessage(p)
+	})
+}
+
+func TestExpiringKeyHasAlreadyExpiredInPatchUpdate(t *testing.T) {
+	streamManagerTest(t, nil, func(p streamManagerTestParams) {
+		p.startStream()
+		p.stream.Enqueue(makePatchEvent(testEnv1))
+
+		_ = p.requireMessage()
+
+		envWithExpiringKey := makeEnvWithAlreadyExpiredKey(testEnv1, oldKey)
+		envWithExpiringKey.Version++
+
+		p.stream.Enqueue(makePatchEvent(envWithExpiringKey))
+
+		msg := p.requireMessage()
+		require.NotNil(t, msg.update)
+		assert.Equal(t, makeEnvironmentParams(testEnv1), *msg.update)
+		assert.Equal(t, config.SDKKey(""), msg.update.ExpiringSDKKey)
+
+		expectNoKeyExpiryMessage(p)
 	})
 }
