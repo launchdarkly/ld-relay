3a51c68c782db3747444a75852a67d16b6cd2be2
LaunchDarklyReleaseBot <86431345+LaunchDarklyReleaseBot@users.noreply.github.com>
2021-09-22 11:34:19 -0700

    prepare 6.4.3 release (#153)

--- internal/util/util_test.go
+++ internal/util/util_test.go
@@ -10,3 +10,10 @@ func TestErrorJSONMsg(t *testing.T) {
 	assert.Equal(t, `{"message":"sorry"}`, string(ErrorJSONMsg("sorry")))
 	assert.Equal(t, `{"message":"bad thing"}`, string(ErrorJSONMsgf("bad %s", "thing")))
 }
+
+func TestRedactURL(t *testing.T) {
+	assert.Equal(t, "redis://redishost:3000", RedactURL("redis://redishost:3000"))
+	assert.Equal(t, "redis://redishost:3000/1", RedactURL("redis://redishost:3000/1"))
+	assert.Equal(t, "redis://username@redishost", RedactURL("redis://username@redishost"))
+	assert.Equal(t, "redis://username:xxxxx@redishost", RedactURL("redis://username:very-secret-password@redishost"))
+}
