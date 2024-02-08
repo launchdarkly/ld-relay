602a24d59fde91c49c5984ec11f95c1f80d147af
Casey Waldren <cwaldren@launchdarkly.com>
2024-02-08 08:59:14 -0800

    chore(v8): remove releaser configuration (#290)
    This commit removes the `.ldrelease` directory. The release process is
    replaced with `release-please` Github Action.

--- .ldrelease/update-version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# See comments in .ldrelease/config.yml for an explanation of the build/release process.
-
-VERSION_GO=relay/version/version.go
-VERSION_GO_TEMP=${VERSION_GO}.tmp
-sed "s/const Version =.*/const Version = \"${LD_RELEASE_VERSION}\"/g" ${VERSION_GO} > ${VERSION_GO_TEMP}
-mv ${VERSION_GO_TEMP} ${VERSION_GO}
