9ea48b513646c80771a5adfbb1f2e2f356d18a6a
Casey Waldren <cwaldren@launchdarkly.com>
2024-02-09 14:38:15 -0800

    feat(v7): build Docker images for ARMv7 and ARM64v8 architectures (#300)
    This commit modifies our goreleaser configuration to achieve two goals: 
    1) Produce release binaries for ARM/ARM64 architectures 
    2) Produce individual docker images for ARM/ARM64, and produce a final
    docker manifest referencing those images.

--- scripts/run-goreleaser.sh
+++ scripts/run-goreleaser.sh
@@ -6,10 +6,6 @@
 # This also pushes the image to DockerHub unless we have specifically told it not to with
 # the --skip-publish option.
 
-export GOPATH=$(mktemp -d)
-export GOBIN=$GOPATH/bin
-go install github.com/goreleaser/goreleaser
-
 # Get the lines added to the most recent changelog update (minus the first 2 lines)
 RELEASE_NOTES=`(GIT_EXTERNAL_DIFF='bash -c "diff --unchanged-line-format=\"\" $2 $5" || true' git log --ext-diff -1 --pretty= -p CHANGELOG.md)`
 
@@ -17,4 +13,4 @@ RELEASE_NOTES=`(GIT_EXTERNAL_DIFF='bash -c "diff --unchanged-line-format=\"\" $2
 # we want it to start from a clean state even if we've previously run a build - and also
 # because during a release, we may need to run this command under another account and we
 # don't want to mess up file permissions in the regular GOPATH.
-"$GOBIN"/goreleaser --clean --release-notes <(echo "${RELEASE_NOTES}") "$@"
+go run github.com/goreleaser/goreleaser@v1.23.0 --clean --release-notes <(echo "${RELEASE_NOTES}") "$@"
