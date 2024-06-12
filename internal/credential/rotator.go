6c29d1785d99b8c8e624901c31fff6f868d60195
Casey Waldren <cwaldren@launchdarkly.com>
2024-06-12 14:43:10 -0700

    tests

--- internal/credential/rotator.go
+++ internal/credential/rotator.go
@@ -15,13 +15,15 @@ type Rotator struct {
 	loggers     ldlog.Loggers
 	timers      map[SDKCredential]*deprecatedCred
 	expirations chan SDKCredential
+	now         func() time.Time
 }
 
-func NewRotator(loggers ldlog.Loggers) *Rotator {
+func NewRotator(loggers ldlog.Loggers, now func() time.Time) *Rotator {
 	r := &Rotator{
 		loggers:     loggers,
 		timers:      make(map[SDKCredential]*deprecatedCred),
 		expirations: make(chan SDKCredential),
+		now:         now,
 	}
 	return r
 }
@@ -35,17 +37,31 @@ func (r *Rotator) Deprecated(cred SDKCredential) bool {
 	return ok
 }
 
-func (r *Rotator) Deprecate(cred SDKCredential, expiry time.Time) {
+func (r *Rotator) Expired(cred SDKCredential) bool {
+	if state, ok := r.timers[cred]; ok {
+		return state.expired
+	}
+	return false
+}
+
+func (r *Rotator) Stop() {
+	for _, state := range r.timers {
+		state.timer.Stop()
+	}
+}
+
+func (r *Rotator) Deprecate(cred SDKCredential, expiry time.Time) bool {
 	if existing, ok := r.timers[cred]; ok {
-		r.loggers.Warnf("Credential %s was marked for deprececation with an expiry time of %v, but it previously expired at %v", cred.Masked(), expiry, existing.expiry)
-		return
+		r.loggers.Warnf("Credential %s was marked for deprecation with an expiry time of %v, but it previously expired at %v", cred.Masked(), expiry, existing.expiry)
+		return false
 	}
 	r.loggers.Infof("Credential %s has been marked for deprecation with an expiry time of %v", cred.Masked(), expiry)
 	state := &deprecatedCred{expired: false}
-	state.timer = time.AfterFunc(expiry.Sub(time.Now()), func() {
+	state.timer = time.AfterFunc(expiry.Sub(r.now()), func() {
 		r.loggers.Info("Credential %s has expired", cred.Masked())
-		r.expirations <- cred
 		state.expired = true
+		r.expirations <- cred
 	})
 	r.timers[cred] = state
+	return true
 }
--- internal/credential/rotator_test.go
+++ internal/credential/rotator_test.go
@@ -1,1 +1,48 @@
 package credential
+
+import (
+	"github.com/launchdarkly/go-sdk-common/v3/ldlogtest"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+type testCred string
+
+func (k testCred) Compare(_ AutoConfig) (SDKCredential, Status) {
+	return nil, Unchanged
+}
+
+func (k testCred) GetAuthorizationHeaderValue() string { return "" }
+
+func (k testCred) Defined() bool {
+	return true
+}
+
+func (k testCred) String() string {
+	return string(k)
+}
+
+func (k testCred) Masked() string { return "masked<" + string(k) + ">" }
+
+func TestNewRotator(t *testing.T) {
+	mockLog := ldlogtest.NewMockLog()
+	rotator := NewRotator(mockLog.Loggers, time.Now)
+	assert.NotNil(t, rotator)
+}
+
+func TestDeprecation(t *testing.T) {
+	mockLog := ldlogtest.NewMockLog()
+
+	now := time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC)
+	future := now.Add(24 * time.Hour)
+
+	rotator := NewRotator(mockLog.Loggers, func() time.Time { return now })
+	defer rotator.Stop()
+
+	cred := testCred("foobar-1234")
+
+	assert.True(t, rotator.Deprecate(cred, future))
+	assert.True(t, rotator.Deprecated(cred))
+	assert.False(t, rotator.Expired(cred))
+}
