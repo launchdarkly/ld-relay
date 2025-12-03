be46dc7aa0b2974df273af6e3d51359b55f909c6
Matthew Keeler <mkeeler@launchdarkly.com>
2025-12-03 10:29:00 -0500

    chore: Bump to 9.0.0-alpha.2

--- relay/version/version.go
+++ relay/version/version.go
@@ -2,4 +2,4 @@
 package version
 
 // Version is the package version
-const Version = "9.0.0-alpha.1" // {{ x-release-please-version }}
+const Version = "9.0.0-alpha.2" // {{ x-release-please-version }}
